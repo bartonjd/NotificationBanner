62286265e989c6e1710cb93c9089b178bf2cbb4e
Josh <Josh@WAYLETH-ALPHA>
2023-07-24 22:42:14 -0600

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management;
+using System.Diagnostics;
+
+namespace LogonAcceptanceWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class DpiDecorator : Decorator
+    {
+        public DpiDecorator()
+        {
+            this.Loaded += (s, e) =>
+            {
+                System.Windows.Media.Matrix m = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
+                ScaleTransform dpiTransform = new ScaleTransform(1 / m.M11, 1 / m.M22);
+                if (dpiTransform.CanFreeze)
+                    dpiTransform.Freeze();
+                this.LayoutTransform = dpiTransform;
+            };
+        }
+    }
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+            //Closing += OnClosing;
+            //StateChanged += OnStateChanged;
+            //Deactivated += (sender, args) => Activate();
+            //LostFocus += (sender, args) => Focus();
+            //LostMouseCapture += (sender, args) => Mouse.Capture(this);
+            //LostKeyboardFocus += (sender, args) => Keyboard.Focus(this);
+            //PreviewLostKeyboardFocus += (sender, args) => Keyboard.Focus(this);
+            
+
+        }
+
+        public double[] GetDpiScale() {
+            Matrix m = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
+            double dpiX = m.M11;
+            double dpiY = m.M22;
+
+            return new[] { dpiX , dpiY};
+            
+        }
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Width = SystemParameters.PrimaryScreenWidth;
+            Height = SystemParameters.PrimaryScreenHeight;
+            double sr = Width / Height;
+            double br = LayBodyArea.ActualWidth / 280;
+
+            //var currentMonitor = Screen.FromHandle(new System.Windows.Interop.WindowInteropHelper(Application.Current.MainWindow).Handle);
+
+            /*MessageBox.Show("The font is: " + NoticeText + " and DPI is: " , "Ratio", MessageBoxButton.OK, MessageBoxImage.Information);*/
+            //NoticeTitleText.FontSize = NoticeTitleText.FontSize * 1.25;
+
+            NoticeTitleText.FontSize = NoticeTitleText.FontSize * 1.25;
+                        NoticeText.FontSize = NoticeText.FontSize * 1.25 ;
+                        NoticeText.MaxWidth = NoticeText.MaxWidth * 1.25 ;
+            //Get background color, lighten and use as button color 
+            Color bg = (NoticeBanner.Background as SolidColorBrush).Color;
+            Color newBg = Utils.AdjustColorBrightness((System.Drawing.Color.FromArgb(bg.A, bg.R, bg.G, bg.B)), .18);
+            AcceptBtn.Background = new SolidColorBrush(newBg);
+            NoticeBanner.Visibility = Visibility.Visible;
+            //AcceptBtn.Foreground = Utils.AdjustColorBrightness(NoticeBanner.Background.,0.25);
+
+
+            //WindowStyle = WindowStyle.None;
+            //WindowState = WindowState.Maximized;
+            //Topmost = true;
+            // Other stuff here
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void OnStateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                WindowState = WindowState.Maximized;
+        }
+
+        private void AcceptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
---
+++ Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Media;
+
+using System.Threading.Tasks;
+
+namespace LogonAcceptanceWindow
+{
+    public class Utils
+    {
+        public static System.Windows.Media.Color AdjustColorBrightness(System.Drawing.Color color, double adjustment)
+    {
+        int r = color.R;
+        int g = color.G;
+        int b = color.B;
+        r += (int)Math.Floor(adjustment * r);
+            if (r > 255) r = 255;
+            if (r < 0) r = 0;
+            b += (int)Math.Floor(adjustment * g);
+            if (b > 255) b = 255;
+            if (b < 0) b = 0;
+            g += (int)Math.Floor(adjustment * b);
+            if (g > 255) g = 255;
+            if (g < 0) g = 0;
+
+            System.Drawing.Color result = System.Drawing.Color.FromArgb(255, r, g, b);
+            return System.Windows.Media.Color.FromRgb(result.R, result.G, result.B);
+
+    }
+}
+}
